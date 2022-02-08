2587daf180a7855809b6e9517b52102a21d9c3c5
Eldridge Turambi <eturambi95@gmail.com>
2022-02-08 14:06:19 -0700

    Add facotry bot and faker gems

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ end
 group :test do
   gem 'rspec-rails'
   gem 'shoulda-matchers'
+  gem 'factory_bot_rails'
+  gem 'faker'
 end
 
 
---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchants API' do
+
+  it 'sends a list of merchants' do
+    binding.pry 
+  end
+end
