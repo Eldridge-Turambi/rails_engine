8bb1bcd13c4560fcef9c70548c2b668a554b1516
Eldridge Turambi <eturambi95@gmail.com>
2022-02-08 23:50:59 -0700

    Added Serialization to Merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,14 +10,16 @@ RSpec.describe 'Merchants API' do
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body, symbolize_names: true)
-    expect(merchants.count).to eq(3)
 
-    merchants.each do |merchant|
+    expect(merchants[:data].count).to eq(3)
+    x = merchants[:data]
+
+    x.each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:id]).to be_a(String)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
 end
